2d2796947e8bf50afaa4549ae7ba622e52e1686b
Nelson Parreira <nelson@ahmic.be>
2017-07-25 14:07:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!-- Based on the video: https://www.youtube.com/watch?v=YXKCNgfdAAM&list=PLfdtiltiRHWGKUvioJly40RJZchSG2-34 -->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP Authentication</title>
+    </head>
+    <body>
+
+    </body>
+</html>
